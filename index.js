33e42a7bdc6e1f0d8ddadd51fddb0b3e29b10c25
hassan <mijuahmed5@gmail.com>
2022-05-09 23:03:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t3cas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("moon-warehouse").collection("product");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.post("/product", (req, res) => {
+      console.log(req.body);
+      db.collection("quotes").insertOne(req.body, (err, data) => {
+        if (err) return console.log(err);
+        res.send("saved to db: " + data);
+      });
+    });
+
+    const result = await productCollection.updateOne(
+      filter,
+      updateDoc,
+      options
+    );
+    res.send(result);
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+//root api
+app.get("/", (req, res) => {
+  res.send("Running Moon Warehouse");
+});
+
+app.listen(port, () => {
+  console.log("CRUD is working", port);
+});
